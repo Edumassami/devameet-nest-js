4a96bd86a551bb6da76cb2083adbe5a89adbe019
Edumassami <du.massami@outlook.com>
2023-08-18 23:15:08 -0300

    Aula 2 - parte 2

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+    AUTH_LOGIN_NOT_FOUND: 'Login inválido',
+    AUTH_PASSWORD_NOT_FOUND: 'Senha inválida',
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'E-mail e/ou senha não encontrados',
+}
